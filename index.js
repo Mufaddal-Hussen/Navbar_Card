d62a9e0578893a9256ed8b532371ad694cf77aff
Mufaddal Hussen <84439430+Mufaddal-Hussen@users.noreply.github.com>
2022-01-20 11:44:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -19,3 +19,45 @@ function togglePopup()
 {
     document.getElementById("popup-1").classList.toggle("active");
 }
+
+
+function showUplod()
+{
+    var idproof = document.getElementById("idprof").value;
+    if(idproof == "passport")
+    {
+    document.getElementById("passportupload").style.display="flex"
+    document.getElementById("adharupload").style.display="none"
+    document.getElementById("voteridupload").style.display="none"
+    }
+    else if(idproof == "Voter Id")
+    {
+        document.getElementById("voteridupload").style.display="flex"
+        document.getElementById("adharupload").style.display="none"
+        document.getElementById("passportupload").style.display="none"
+
+    }
+    else if(idproof == "Aadhar Card")
+    {
+        document.getElementById("adharupload").style.display="flex"
+        document.getElementById("passportupload").style.display="none"
+        document.getElementById("voteridupload").style.display="none"
+
+    }
+    else
+    {
+        document.getElementById("adharupload").style.display="none"
+        document.getElementById("passportupload").style.display="none"
+        document.getElementById("voteridupload").style.display="none"
+    }
+}
+    // if(a==1)
+    // {
+    //     document.getElementById("uploaddcmnt").style.display="inline"
+    //     return a=0;
+    // }
+    // else
+    // {
+    //     document.getElementById("uploaddcmnt").style.display="none"
+    //     return a=1;
+    // }
